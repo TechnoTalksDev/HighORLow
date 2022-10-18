b8aab8ff1ac29e3df1edc1fd941018ef7cae4d6a
TechnoTalksDev <technotalksyt101@gmail.com>
2022-10-17 19:29:34 -0700

    Inital Comit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1><span class="hot">High</span> <span class="or">OR</span> <span class="cold">Low</span></h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+  
+  <p>
+    A simple high or low game!
+  </p>
+
+  <footer class="read-the-docs">
+    <p>
+      Powered by Vite + Svelte
+    </p>
+      <!-- svelte-ignore security-anchor-rel-noreferrer -->
+      <a href="https://vitejs.dev" target="_blank"> 
+        <img src="/vite.svg" class="logo" alt="Vite Logo" />
+      </a>
+      <!-- svelte-ignore security-anchor-rel-noreferrer -->
+      <a href="https://svelte.dev" target="_blank"> 
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+  </footer>
+</main>
+
+<style>
+  .logo {
+    height: 10%;
+    padding: .1em 0.5em 0.1em 0.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+  .hot {
+    color: red;
+    filter: drop-shadow(0 0 2.5px red);
+  }
+  .hot:hover {
+    filter: drop-shadow(0 0 5px red);
+    transition: 0.5s;
+  }
+  .hot:not(:hover) {
+    transition: 0.5s;
+  }
+  .cold {
+    color: skyblue;
+    filter: drop-shadow(0 0 2.5px skyblue);
+  }
+  .cold:hover {
+    filter: drop-shadow(0 0 5px skyblue);
+    transition: 0.5s;
+  }
+  .cold:not(:hover) {
+    transition: 0.5s;
+  }
+  .or:hover {
+    filter: drop-shadow(0 0 2.5px white);
+    transition: 0.5s;
+  }
+  .or:not(:hover) {
+    transition: 5s;
+  }
+</style>
